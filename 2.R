7c15f691d97a78412925ef6cf803ea3cdc61e1bb
mhumphries23 <michael.humphries@nomissolutions.com>
2016-08-04 17:13:04 +0100

    Add files via upload

---
+++ 2.R
@@ -0,0 +1,9 @@
+temp<-tempfile()
+download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip",temp)
+data <- read.table(unz(temp,"household_power_consumption.txt"),header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
+data<-data[data$Date %in% c("1/2/2007","2/2/2007") ,]
+datetime <- strptime(paste(data$Date, data$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
+globalActivePower <- as.numeric(data$Global_active_power)
+png("plot2.png", width=480, height=480)
+plot(datetime, globalActivePower, type="l", xlab="", ylab="Global Active Power (kilowatts)")
+dev.off()
